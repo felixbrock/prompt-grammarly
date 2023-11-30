ede7efcda8b356a08cc4ab9daebd0066ec88c9b6
Felix Brockmeier <fe.brockmeier@gmail.com>
2023-11-30 19:21:20 +0000

    Add multi assistant logic

--- main/internal/app/routes.go
+++ main/internal/app/routes.go
@@ -3,11 +3,13 @@ package app
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 	"strings"
+	"time"
 
 	apphandler "github.com/felixbrock/lemonai/internal/appHandler"
 )
@@ -22,7 +24,8 @@ type RunProto struct {
 }
 
 type Run struct {
-	Id string `json:"id"`
+	Id     string `json:"id"`
+	Status string `json:"status"`
 }
 
 type MessageProto struct {
@@ -41,6 +44,7 @@ type MessageContent struct {
 type Message struct {
 	Id      string           `json:"id"`
 	Content []MessageContent `json:"content"`
+	Role    string           `json:"role"`
 }
 
 type MessageListing struct {
@@ -51,7 +55,28 @@ type Thread struct {
 	Id string `json:"id"`
 }
 
-func request(method string, url string, headerProtos []string, reqBody []byte) (*Response, error) {
+type ImprovementSuggestion struct {
+	Original  string `json:"original"`
+	Improved  string `json:"new"`
+	Reasoning string `json:"reasoning"`
+}
+
+type Improvement struct {
+	OriginalPrompt         string
+	ImprovementSuggestions []ImprovementSuggestion
+	ImprovedPrompt         *string
+}
+
+type assistant struct {
+	Id   string
+	Name string
+}
+
+type chatRequest struct {
+	Prompt string `json:"prompt"`
+}
+
+func request[T any](method string, url string, headerProtos []string, reqBody []byte) (*T, error) {
 	req, err := http.NewRequest(method, url, bytes.NewBuffer(reqBody))
 
 	if err != nil {
@@ -67,75 +92,49 @@ func request(method string, url string, headerProtos []string, reqBody []byte) (
 
 	if err != nil {
 		return nil, err
+	} else if resp.StatusCode != 200 {
+		return nil, errors.New("unexpected response status code error")
 	}
 
-	defer func() {
-		err = resp.Body.Close()
-		if err != nil {
-			slog.Error(fmt.Sprintf(`Error occured: %s`, err.Error()))
-		}
-	}()
-
-	var respBody []byte
-	respBody, err = io.ReadAll(resp.Body)
+	var t *T
+	t, err = readJSON[T](resp.Body)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &Response{Body: respBody, StatusCode: resp.StatusCode}, nil
+	return t, nil
 }
 
-func getAssistantId(assistantName string) (string, error) {
-	// TODO: retrieve assistant id from OAI and cache
+func postRun(threadId string, runId string, headerProtos []string) (*Run, error) {
+	url := fmt.Sprintf(`https://api.openai.com/v1/threads/%s/runs/%s`, threadId, runId)
 
-	switch assistantName {
-	case "ContextualRichness":
-		return "asst_BxUQqxSD8tcvQoyR6T5iom3L", nil
-	case "Conciseness":
-		return "asst_3q6LvmiPZyoPChdrcuqMxOvh", nil
-	case "Clarity":
-		return "asst_8IjCbTm7tsgCtSbhEL7E7rjB", nil
-	case "Consistency":
-		return "asst_221Q0E9EeazCHcGV4Qd050Gy", nil
-	case "Operator":
-		return "asst_qUn97Ck3zzdvNToMVAMhNzTk", nil
-	default:
-		return "", fmt.Errorf("assistant %s not found", assistantName)
+	run, err := request[Run]("GET", url, headerProtos, nil)
+
+	if err != nil {
+		return nil, err
 	}
+
+	return run, nil
 }
 
-func createRun(runProto RunProto, threadId string, headerProtos []string) (*string, error) {
+func createRun(runProto RunProto, threadId string, headerProtos []string) (*Run, error) {
 	body := []byte(fmt.Sprintf(`{"assistant_id": "%s"}`, runProto.AssistantId))
 	url := fmt.Sprintf(`https://api.openai.com/v1/threads/%s/runs`, threadId)
 
-	resp, err := request("POST", url, headerProtos, body)
-
-	if err != nil || resp.StatusCode != 200 {
-		return nil, err
-	}
-
-	var run Run
-	err = json.Unmarshal(resp.Body, &run)
+	run, err := request[Run]("POST", url, headerProtos, body)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &run.Id, nil
+	return run, nil
 }
 
 func listMsgs(threadId string, headerProtos []string) (*[]Message, error) {
 	url := fmt.Sprintf(`https://api.openai.com/v1/threads/%s/messages`, threadId)
 
-	resp, err := request("GET", url, headerProtos, nil)
-
-	if err != nil || resp.StatusCode != 200 {
-		return nil, err
-	}
-
-	var msgs MessageListing
-	err = json.Unmarshal(resp.Body, &msgs)
+	msgs, err := request[MessageListing]("GET", url, headerProtos, nil)
 
 	if err != nil {
 		return nil, err
@@ -148,14 +147,7 @@ func createMsg(msgProto MessageProto, threadId string, headerProtos []string) (*
 	body := []byte(fmt.Sprintf(`{"role": "%s", "content": %s}`, msgProto.Role, msgProto.Content))
 	url := fmt.Sprintf(`https://api.openai.com/v1/threads/%s/messages`, threadId)
 
-	resp, err := request("POST", url, headerProtos, body)
-
-	if err != nil || resp.StatusCode != 200 {
-		return nil, err
-	}
-
-	var msg Message
-	err = json.Unmarshal(resp.Body, &msg)
+	msg, err := request[Message]("POST", url, headerProtos, body)
 
 	if err != nil {
 		return nil, err
@@ -165,34 +157,162 @@ func createMsg(msgProto MessageProto, threadId string, headerProtos []string) (*
 }
 
 func createThread(headerProtos []string) (*string, error) {
-	resp, err := request("POST", "https://api.openai.com/v1/threads", headerProtos, nil)
-
-	if err != nil || resp.StatusCode != 200 {
-		return nil, err
-	}
-
-	var thread Thread
-	err = json.Unmarshal(resp.Body, &thread)
+	resp, err := request[Thread]("POST", "https://api.openai.com/v1/threads", headerProtos, nil)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &thread.Id, nil
+	return &resp.Id, nil
 }
 
 func deleteThread(threadId string, headerProtos []string) error {
 	url := fmt.Sprintf(`https://api.openai.com/v1/threads/%s`, threadId)
-	resp, err := request("DELETE", url, headerProtos, nil)
+	_, err := request[Thread]("DELETE", url, headerProtos, nil)
 
-	if err != nil || resp.StatusCode != 200 {
+	if err != nil {
 		return err
 	}
 
 	return nil
 }
 
+func runAssistant(threadId *string, userPrompt string, assistant assistant, headerProtos []string) (*[]byte, error) {
+	err := writeUserPrompt(threadId, userPrompt, headerProtos)
+
+	if err != nil {
+		return nil, err
+	}
+
+	run, err := createRun(RunProto{AssistantId: assistant.Id}, *threadId, headerProtos)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("waiting for %s assistant run to complete...", assistant.Name)
+	for run.Status != "completed" {
+		run, err = postRun(*threadId, run.Id, headerProtos)
+		if err != nil {
+			return nil, err
+		}
+		time.Sleep(1000)
+	}
+	fmt.Printf("completed %s assistant run.", assistant.Name)
+
+	var msgs *[]Message
+	msgs, err = listMsgs(*threadId, headerProtos)
+
+	if err != nil {
+		return nil, err
+	}
+
+	msg := (*msgs)[len(*msgs)-1]
+	if msg.Role != "assistant" || len(msg.Content) != 1 {
+		return nil, errors.New("unexpected assistant response error")
+	}
+
+	bMsg := []byte(msg.Content[0].Text.Value)
+	return &bMsg, nil
+}
+
+func writeUserPrompt(threadId *string, prompt string, headerProtos []string) error {
+	msgContent, err := json.Marshal(prompt)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = createMsg(MessageProto{Role: "user", Content: msgContent}, *threadId, headerProtos)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func genAssistantUserPrompt(assistantName string, prompt []byte) string {
+	return fmt.Sprintf(
+		`Evaluate the %s of the following model instruction:
+
+		%s
+		`, strings.ToLower(assistantName), prompt)
+}
+
+func genOperatorUserPrompt(assistantName string, prompt []byte) string {
+	return fmt.Sprintf(
+		`Apply the following list of suggestions to improve contextual richness to the following model instructions.
+
+		Model Instructions:
+
+		%s
+
+
+		Suggestions:
+
+		%s
+		`, strings.ToLower(assistantName), prompt)
+}
+
+func improve(threadId *string, prompt []byte, targetAssistant assistant, headerProtos []string) (*[]byte, error) {
+	userPrompt := genAssistantUserPrompt(targetAssistant.Name, prompt)
+
+	msg, err := runAssistant(threadId, userPrompt, targetAssistant, headerProtos)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf(`%s >> %s`, targetAssistant.Name, *msg)
+
+	operator := assistant{Id: "asst_qUn97Ck3zzdvNToMVAMhNzTk", Name: "Operator"}
+
+	userPrompt = genOperatorUserPrompt(operator.Name, *msg)
+
+	msg, err = runAssistant(threadId, userPrompt, operator, headerProtos)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf(`%s >> %s`, operator.Name, *msg)
+
+	return msg, nil
+}
+
+func readJSON[T any](reader io.ReadCloser) (*T, error) {
+	var err error
+
+	defer func() {
+		err = reader.Close()
+		if err != nil {
+			slog.Error(fmt.Sprintf(`Error occured: %s`, err.Error()))
+		}
+	}()
+
+	var content []byte
+	content, err = io.ReadAll(reader)
+
+	if err != nil {
+		return nil, err
+	} else if len(content) == 0 {
+		return nil, errors.New("no reader content error")
+	}
+
+	var t *T
+	err = json.Unmarshal(content, &t)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
 func chat(w http.ResponseWriter, r *http.Request) *apphandler.AppError {
+	chatReq, err := readJSON[chatRequest](r.Body)
+
 	headerProtos := []string{
 		"Content-Type:application/json",
 		"Authorization:Bearer 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",
@@ -212,46 +332,24 @@ func chat(w http.ResponseWriter, r *http.Request) *apphandler.AppError {
 		return &apphandler.AppError{Error: err, Message: "Service temporarily unavailable.", Code: 500}
 	}
 
-	var msgContent []byte
-	msgContent, err = json.Marshal(`Evaluate the contextual richness of the following model instruction:
-	You are an AI research assistant designed to aid qualitative research consultants working on a project titled "Chocolate".
+	assistants := []assistant{{Id: "asst_BxUQqxSD8tcvQoyR6T5iom3L", Name: "Contextual Richness"},
+		{Id: "asst_3q6LvmiPZyoPChdrcuqMxOvh", Name: "Conciseness"},
+		{Id: "asst_8IjCbTm7tsgCtSbhEL7E7rjB", Name: "Clarity"},
+		{Id: "asst_221Q0E9EeazCHcGV4Qd050Gy", Name: "Consistency"}}
 
-	You must ensure that all responses are directly drawn from the user's research data. Your responses should never be derived from general knowledge or made up, even when the question seems mundane or the topic appears trivial.
-	
-	If the user's research data doesn't contain the information to answer the question, you should inform the user that the research data does not have the specific answer, and refrain from generating a response based on general knowledge or assumptions. Always prioritize the user's specific research context in your responses. This is of utmost importance.
-	`)
+	prompt := []byte(chatReq.Prompt)
+	for i := 0; i < len(assistants); i++ {
+		var tempPrompt *[]byte
+		tempPrompt, err = improve(threadId, prompt, assistants[i], headerProtos)
 
-	if err != nil {
-		return &apphandler.AppError{Error: err, Message: "Service temporarily unavailable.", Code: 500}
+		if err != nil {
+			return &apphandler.AppError{Error: err, Message: "Service temporarily unavailable.", Code: 500}
+		}
+
+		prompt = *tempPrompt
 	}
 
-	_, err = createMsg(MessageProto{Role: "user", Content: msgContent}, *threadId, headerProtos)
-
-	if err != nil {
-		return &apphandler.AppError{Error: err, Message: "Service temporarily unavailable.", Code: 500}
-	}
-
-	var assistantId string
-	assistantId, err = getAssistantId("ContextualRichness")
-
-	if err != nil {
-		return &apphandler.AppError{Error: err, Message: "Service temporarily unavailable.", Code: 500}
-	}
-
-	createRun(RunProto{AssistantId: assistantId}, *threadId, headerProtos)
-
-	var msgs *[]Message
-	msgs, err = listMsgs(*threadId, headerProtos)
-
-	if err != nil {
-		return &apphandler.AppError{Error: err, Message: "Service temporarily unavailable.", Code: 500}
-	}
-
-	content := (*msgs)[len(*msgs)-1].Content
-	if len(content) != 0 {
-		fmt.Fprint(w, content[0].Text.Value)
-		return nil
-	}
+	fmt.Fprint(w, prompt)
 
 	return nil
 	// tmpl := template.Must(template.ParseFiles("./templates/index.html"))
